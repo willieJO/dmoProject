a8aa440b40f696c723409091e7255e519df8d18c
Otuski2706 <gabriel.otuski@ifsp.edu.br>
2023-05-22 21:59:46 -0300

    layout-cadastros

---
+++ app/src/main/java/br/edu/ifsp/dmo5/alugel/carros/NewLocadorActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.dmo5.alugel.carros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewLocadorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_locador);
+    }
+}
